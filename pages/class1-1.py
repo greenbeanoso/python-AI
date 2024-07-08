840fc315b19db4a8c33811482331d636feeca835
greenbeanoso <osogreenban@gmail.com>
2024-07-08 16:04:10 +0800

    筆記更新 網站標題

--- pages/class1-1.py
+++ pages/class1-1.py
@@ -7,17 +7,55 @@ print("hELlO wORLD?")  # 印出hello world
 # if True: # ctrl + / 可以注解
 #     pass
 
-# 下午
-print("a")  # 字串str
-print("1")  #
+# # 下午
+# print("a")  # 字串str
+# print("1")  #
 
-print(1)  # 數字int 最大值2**31-1 最小值-2**31
+# print(1)  # 數字int 最大值2**31-1 最小值-2**31
 
-print(1.123)  # 浮點數float
-print(1.0)  #
+# print(1.123)  # 浮點數float
+# print(1.0)  #
 
-print(True)  # 布林值bool    True or False
+# print(True)  # 布林值bool    True or False
 
-# 為什麼需要兩種字串符號呢？
-print("'")  # 單引號字串
-print('"')  # 雙引號字串
+# # 為什麼需要兩種字串符號呢？
+# print("'")  # 單引號字串
+# print('"')  # 雙引號字串
+
+# #常用數學運算子
+# print(1 + 1)  # 加法
+# print(1 - 1)  # 減法
+# print(1 * 1)  # 乘法
+# print(1 / 1)  # 除法
+# print(1 // 1)  # 整除
+# print(1 % 1)  # 取餘數
+# print(1**1)  # 次方 指數
+# # 以上符號符號的優先順序
+# # 1.()
+# # 2.**
+# # 3. * / // %
+# # 4. + -
+
+# # 字串運算
+# a = "hello"  # 字串
+# b = "world"  # 字串
+# print(a + b)  # 字串加法 helloworld
+# print(a * 3)  # 字串乘法 hellohellohello
+# print(a + b * 3)  # 運算子優先順序 helloworldworldworld
+# print(a[0])  # 字串索引  h
+# print(a[0:2])  # 字串索引 he
+# print(a[0:2] + b[1:3])  # 字串索引 heor
+
+# print(f"free {1+1} play")  # f-string 格式化字串
+# # f-string 格式化字串:可以在字串中用{}包起來 使用不同型態的變數
+
+# print(len("hello"))  # 字串長度
+
+# print(int("1"))  # 字串轉數字
+# print(float("1.1"))  # 字串轉浮點數
+# print(bool("True"))  # 字串轉布林值
+# print(str(1))  # 數字轉字串
+
+a = input("請輸入你的名字：")  # a是回傳值 input可以讓用戶輸入
+print(type(a))  # input回傳值的類型
+# input可以讓用戶輸入 回傳值為字串
