d57549694998c29f25cc9acfd0c70cbe49be3396
greenbeanoso <osogreenban@gmail.com>
2024-07-08 13:51:57 +0800

    空

--- main.py
+++
@@ -1,8 +0,0 @@
-print("hELlO wORLD?")  # 印出hello world
-# class 1  註解筆記
-# 這是單行註解
-"""
-這是多行註解
-"""
-# if True: # ctrl + / 可以注解
-#     pass
---
+++ pages/class1-1.py
@@ -0,0 +1,22 @@
+print("hELlO wORLD?")  # 印出hello world
+# class 1  註解筆記
+# 這是單行註解
+"""
+這是多行註解
+"""
+# if True: # ctrl + / 可以注解
+#     pass
+
+print("a")  # 字串str
+print("1")  #
+
+print(1)  # 數字int 最大值2**31-1 最小值-2**31
+
+print(1.123)  # 浮點數float
+print(1.0)  #
+
+print(True)  # 布林值bool    True or False
+
+# 為什麼需要兩種字串符號呢？
+print("'")  # 單引號字串
+print('"')  # 雙引號字串
